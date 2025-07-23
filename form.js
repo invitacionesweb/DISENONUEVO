ff1c8adbb2ee9bf9ec05b6edf8080df1a1c6b4d6
invitacionesweb <invitacionesweb.info@gmail.com>
2025-07-23 13:50:37 -0700

    acac
    ac

---
+++ form.js
@@ -0,0 +1,14 @@
+const scriptUrl = 'https://script.google.com/macros/s/AKfycbw2ycg-irHpdrDTGdr6Nk5N7RlDGVIIupz75zFzYk6Lf-DjzGpoc_nW6XC3mXPJOAQ/exec';
+const form = document.forms['asistenciaform'];
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    fetch(scriptUrl, {method: 'POST', body: new FormData(form)})
+            .then(response => Swal.fire({
+        title: "¡MUCHAS GRACIAS!",
+        text: "Formulario Envíado",
+        icon: "success"}))
+            .then( () => { window.location.reload() } )
+            .catch(error => console.error('Error', error.message))
+})
